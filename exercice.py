55daef98ef9a15cc7a1e2dea1bdd4c9b148bcc85
Raouf.Tb <raouf1704@outlook.com>
2021-09-25 13:46:32 -0400

    Exercice

--- exercice.py
+++ exercice.py
@@ -6,30 +6,66 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number >= 0:
+        result = number
+    else:
+        result = -1 * number
+    return result 
+
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-
-    return [""]
+    for lettre in prefixes:
+        result = (lettre + suffixe)
+    return result
 
 
 def prime_integer_summation() -> int:
-    return 0
+    somme = 0
+    liste_premier = [2,3,5,7,11,13,7, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                    73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+                    179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+                    283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+                    419, 421, 431, 433,439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541
+                    ]
+    for nombre in liste_premier:
+        somme += nombre
+    return somme
 
 
 def factorial(number: int) -> int:
-    return 0
+    facto = 1
+    for i in range(1, number +1):
+        facto = facto * i
+    return facto
 
 
 def use_continue() -> None:
-    pass
+    for i in range(0,11):
+        if i == 5:
+            continue
+        print(i)
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    accepted = [False for _ in range(len(groups))]
+        
+    for position in range(len(groups)):
+        if len(groups[position]) > 10 or len(groups[position])<=3:
+                pass
+        else:
+            if any(element == 25 for element in groups[position]):
+                accepted[position] = True
 
+            elif any(element < 18 for element in groups[position]):
+                pass
+            elif any(element > 70 for element in groups[position]) and any(element ==50 for element in groups[position]):
+                pass
+            else: 
+                accepted[position] = True
+
+    return accepted
 
 def main() -> None:
     number = -4.325
